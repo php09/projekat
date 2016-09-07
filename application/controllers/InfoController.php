fc7397ac50c43b0f9090eacb321fdc968d5f30bc
php09 <php09@users.noreply.github.com>
2016-09-07 16:11:10 +0200

    Fixed enable action in admin/sitemap Finished aboutus action in info controller.

--- application/controllers/InfoController.php
+++ application/controllers/InfoController.php
@@ -8,6 +8,28 @@ class InfoController extends Zend_Controller_Action{
     
     public function aboutusAction() {
         
+        $id = (int) $this->getRequest()->getParam("sitemap_page_id");
+        
+        if($id <= 0) {
+            throw new Zend_Controller_Router_Exception('Invalid parent id ' . $id . ' for sitemap pages.', 404);
+        }
+        
+        $sitemapTable = new Application_Model_DbTable_CmsSitemapPages();
+        
+        $sitemapPage = $sitemapTable->search(
+                array(
+                    'filters' => array(
+                        'type' => 'AboutusPage',
+                        'status' => Application_Model_DbTable_CmsSitemapPages::STATUS_ENABLED,
+                        'id' => $id
+                )));
+        
+        if(!$sitemapPage) {
+            throw new Zend_Controller_Router_Exception('About us page was not found.', 404);
+        }
+        
+        $this->view->sitemapPage = $sitemapPage[0];
+        
     }
     
     public function contactusAction() {
