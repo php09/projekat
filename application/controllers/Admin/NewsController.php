af65656b55e17ab7ab2faef2b28a4cb4a466e98f
php09 <php09@users.noreply.github.com>
2016-09-13 20:15:30 +0200

    Some fixes for admin_news

--- application/controllers/Admin/NewsController.php
+++ application/controllers/Admin/NewsController.php
@@ -26,14 +26,22 @@ class Admin_NewsController extends Zend_Controller_Action {
 //            )
 //                )
 //        );
-       
-        
         //TODO: move this to model!
-        $select = $newsCmsTable->select()->setIntegrityCheck(false)->from("cms_news")->join("cms_users", "cms_users.id = cms_news.author_id");
-        
+//        $select = $newsCmsTable->select()
+//                ->setIntegrityCheck(false)
+//                ->from("cms_news")
+//                ->join("cms_users", "cms_users.id = cms_news.author_id");
+
+        $select = $newsCmsTable->select()
+                ->setIntegrityCheck(false)
+                ->from("cms_users")
+                ->join("cms_news", "cms_users.id = cms_news.author_id")
+                ->order(array('date_posted DESC'));
+
         $news = $newsCmsTable->fetchAll($select)->toArray();
-        
+
         $this->view->news = $news;
+        $this->view->systemMessages = $systemMessages;
     }
 
     public function addAction() {
@@ -69,14 +77,14 @@ class Admin_NewsController extends Zend_Controller_Action {
                 $formData = $form->getValues();
 
                 // set parent_id for new page
-                
+
                 $formData['author_id'] = $user['id'];
 
                 //remove key sitemap_page_photo form form data because there is no column 'sitemap_page_photo' in cms_sitemapPages table
                 //unset($formData['sitemap_page_photo']);
                 //Insertujemo novi zapis u tabelu
                 //insert sitemapPage returns ID of the new sitemapPage
-                $sitemapPageId = $cmsNewsTable->insertNews($formData);
+                
 
                 // do actual task
                 //save to database etc
@@ -104,6 +112,67 @@ class Admin_NewsController extends Zend_Controller_Action {
     }
 
     public function deleteAction() {
+
+        $request = $this->getRequest();
+
+        if (!$request->isPost() || $request->getPost('task') != 'delete') {
+            // request is not post
+            // or task is not delete
+            //redirect to index page
+            //redirect to same or another page
+            $redirector = $this->getHelper('Redirector');
+            $redirector->setExit(true)
+                    ->gotoRoute(array(
+                        'controller' => 'admin_news',
+                        'action' => 'index'
+                            ), 'default', true);
+        }
+
+        $flashMessenger = $this->getHelper('FlashMessenger');
+
+        try {
+
+            // read $_POST['id']
+            $id = (int) $request->getPost('id');
+
+            if ($id <= 0) {
+
+                throw new Application_Model_Exception_InvalidInput('Invalid news id: ' . $id);
+            }
+
+            $cmsNewsTable = new Application_Model_DbTable_CmsNews();
+
+            $news = $cmsNewsTable->getNewsById($id);
+
+            if (empty($news)) {
+                throw new Application_Model_Exception_InvalidInput('No news is found with id: ' . $id);
+            }
+
+            $cmsNewsTable->deleteNews($id);
+
+            $flashMessenger->addMessage('News ' . $news['title'] . ' has been deleted', 'success');
+
+            //redirect to same or another page
+            $redirector = $this->getHelper('Redirector');
+            $redirector->setExit(true)
+                    ->gotoRoute(array(
+                        'controller' => 'admin_news',
+                        'action' => 'index'
+                            ), 'default', true);
+        } catch (Application_Model_Exception_InvalidInput $ex) {
+
+            $flashMessenger->addMessage($ex->getMessage(), 'errors');
+
+            //redirect to same or another page
+            $redirector = $this->getHelper('Redirector');
+            $redirector->setExit(true)
+                    ->gotoRoute(array(
+                        'controller' => 'admin_news',
+                        'action' => 'index'
+                            ), 'default', true);
+        }
+        
+        $this->view->systemMessages = $systemMessages;
         
     }
 
---
+++ application/forms/Admin/NewsEdit.php
@@ -0,0 +1,5 @@
+<?php
+
+class Application_Form_Admin_NewsEdit extends Application_Form_Admin_NewsAdd {
+    
+}
