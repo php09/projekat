96892bfd12d13fa1f8b55b3fe1ab8f6fe5f90e84
php09 <php09@users.noreply.github.com>
2016-09-30 14:38:47 +0200

    finnished ajax for product categories

--- application/controllers/Admin/ManagecategoriesController.php
+++ application/controllers/Admin/ManagecategoriesController.php
@@ -1,161 +1,130 @@
 <?php
 
-class Admin_ManagecategoriesController extends Zend_Controller_Action
-{
-    
-    public function indexAction()
-    {
+class Admin_ManagecategoriesController extends Zend_Controller_Action {
+
+    public function indexAction() {
         //placeholder
         $characteristicId = 1;
-        
+
         $table = new Zend_Db_Table("cms_product_types_product_characteristics");
         $select = $table->select();
         $select->where("product_characteristic_id = ? ", $characteristicId);
         $chars = $table->fetchAll()->toArray();
-        
+
         $this->view->chars = $chars;
-        
     }
-    
-    
+
     public function producttypesAction() {
         Zend_Layout::getMvcInstance()->disableLayout();
 
         $request = $this->getRequest();
         $request instanceof Zend_Controller_Request_Http;
-        
-        if(!$request->isXmlHttpRequest()) {
+
+        if (!$request->isXmlHttpRequest()) {
             $redirector = $this->getHelper('Redirector');
             $redirector->setExit(true)
-                ->gotoRoute(array(
-                    'controller' => 'admin_dashboard',
-                    'action' => 'index'
-                    ), 'default', true);
+                    ->gotoRoute(array(
+                        'controller' => 'admin_dashboard',
+                        'action' => 'index'
+                            ), 'default', true);
         }
         $table = new Zend_Db_Table("cms_product_types");
         $select = $table->select();
         $types = $table->fetchAll()->toArray();
-        
+
         $this->view->types = $types;
     }
-    
+
     public function producttypesdeleteAction() {
         $request = $this->getRequest();
-            
-            if(!$request->isPost() || $request->getPost('task') != 'typedelete' ) {
-                
-                $redirector = $this->getHelper('Redirector');
-                $redirector->setExit(true)
-                            ->gotoRoute(array(
-                                'controller' => 'admin_managecategories',
-                                'action' => 'index'
-                                ), 'default', true);
-            }
-            
-            $flashMessenger = $this->getHelper('FlashMessenger');
-            
-            try {
-                
+
+        if (!$request->isPost() || $request->getPost('task') != 'typedelete') {
+
+            $redirector = $this->getHelper('Redirector');
+            $redirector->setExit(true)
+                    ->gotoRoute(array(
+                        'controller' => 'admin_managecategories',
+                        'action' => 'index'
+                            ), 'default', true);
+        }
+
+        $flashMessenger = $this->getHelper('FlashMessenger');
+
+        try {
+
             $value = $request->getPost("type");
             
-//            if($id <= 0) {
-//                
-//                throw new Application_Model_Exception_InvalidInput("Invalid member id: " . $id );
-//                
-//            }
+            /*
+             * TODO: filtering and validation, backend form creation and validation
+             */
             
+
             $cmsTypeTable = new Application_Model_DbTable_CmsTypes();
-    
+
             $cmsTypeTable->delete("id = " . $value);
-             
-             $this->getHelper("Json")->sendJson( array("status" => "successfully deleted product type"));
 
-            } catch (Application_Model_Exception_InvalidInput $ex) {
+            $this->getHelper("Json")->sendJson(array("status" => "successfully deleted product type"));
+        } catch (Application_Model_Exception_InvalidInput $ex) {
 
-                $flashMessenger->addMessage($ex->getMessage());
-                
-                $redirector = $this->getHelper('Redirector');
-                $redirector->setExit(true)
-                            ->gotoRoute(array(
-                                'controller' => 'admin_managecategories',
-                                'action' => 'index'
-                                ), 'default', true);
-                
-            }
-        
-        
+            $flashMessenger->addMessage($ex->getMessage());
+
+            $redirector = $this->getHelper('Redirector');
+            $redirector->setExit(true)
+                    ->gotoRoute(array(
+                        'controller' => 'admin_managecategories',
+                        'action' => 'index'
+                            ), 'default', true);
+        }
     }
-    
-    public function producttypesinsertAction() {
 
-//        $typesTable = new Application_Model_DbTable_CmsTypes();
-//        $typesTable->insertType("test");
-//        echo "<pre>"; 
-//        var_dump($this->getRequest()->getParams());
-//        die();
+    public function producttypesinsertAction() {
         
         $request = $this->getRequest();
-            
-            if(!$request->isPost() || $request->getPost('task') != 'typeinsert' ) {
-                
-                $redirector = $this->getHelper('Redirector');
-                $redirector->setExit(true)
-                            ->gotoRoute(array(
-                                'controller' => 'admin_managecategories',
-                                'action' => 'index'
-                                ), 'default', true);
-            }
-            
-            $flashMessenger = $this->getHelper('FlashMessenger');
-            
-            try {
-                
+
+        if (!$request->isPost() || $request->getPost('task') != 'typeinsert') {
+
+            $redirector = $this->getHelper('Redirector');
+            $redirector->setExit(true)
+                    ->gotoRoute(array(
+                        'controller' => 'admin_managecategories',
+                        'action' => 'index'
+                            ), 'default', true);
+        }
+
+        $flashMessenger = $this->getHelper('FlashMessenger');
+
+        try {
+
             $value = $request->getPost("type");
             
-//            if($id <= 0) {
-//                
-//                throw new Application_Model_Exception_InvalidInput("Invalid member id: " . $id );
-//                
-//            }
             
+            /*
+             * TODO: filtering and validation, backend form creation and validation
+             */
+            
+            
+
             $cmsTypeTable = new Application_Model_DbTable_CmsTypes();
-    
-                $cmsTypeTable->insertType($value);
-                
-                
-                $flashMessenger->addMessage("Member " . $member["first_name"] . " " . $member["last_name"] . " has been enabled." , "success");
-                
-                $redirector = $this->getHelper('Redirector');
-                $redirector->setExit(true)
-                            ->gotoRoute(array(
-                                'controller' => 'admin_managecategories',
-                                'action' => 'index'
-                                ), 'default', true);
 
-            } catch (Application_Model_Exception_InvalidInput $ex) {
+            $cmsTypeTable->insertType($value);
 
-                $flashMessenger->addMessage($ex->getMessage());
-                
-                $redirector = $this->getHelper('Redirector');
-                $redirector->setExit(true)
-                            ->gotoRoute(array(
-                                'controller' => 'admin_managecategories',
-                                'action' => 'index'
-                                ), 'default', true);
-                
-            }
-        
-        
-        
-        
-        
+            $this->getHelper("Json")->sendJson(array("status" => "successfully inserted product type"));
+        } catch (Application_Model_Exception_InvalidInput $ex) {
+
+            $flashMessenger->addMessage($ex->getMessage());
+
+            $redirector = $this->getHelper('Redirector');
+            $redirector->setExit(true)
+                    ->gotoRoute(array(
+                        'controller' => 'admin_managecategories',
+                        'action' => 'index'
+                            ), 'default', true);
+        }
     }
-    
-    
+
     public function ajaxtestAction() {
         $json = $this->getRequest()->getParams();
         $this->getHelper("Json")->sendJson($json);
     }
-    
-    
+
 }
--- application/forms/Admin/ProductAdd.php
+++ application/forms/Admin/ProductAdd.php
@@ -5,6 +5,31 @@ class Application_Form_Admin_ProductAdd extends Zend_Form
     
     public function init() {
         
+        $title = new Zend_Form_Element_Text("title");
+        $title;
+        $this->addElement($title);
+        
+        $class = new Zend_Form_Element_Select("class");
+        $class;
+        $this->addElement($class);
+        
+        $type = new Zend_Form_Element_Select("type");
+        $type;
+        $this->addElement($type);
+        
+        $vendor = new Zend_Form_Element_Text("vendor");
+        $vendor;
+        $this->addElement($vendor);
+        
+        $model = new Zend_Form_Element_Text("model");
+        $model;
+        $this->addElement($model);
+        
+        $characteristics = new Zend_Form_Element_Text("characteristics");
+        $characteristics; 
+       $this->addElement($characteristics);
+        
+                
     }
     
 }
